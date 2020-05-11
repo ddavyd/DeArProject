7a897bb57f793f81c6743dc7afb72934b0f8a7d4
Denis <denis.davydov.a@mail.ru>
2020-05-11 16:43:28 +0300

    img hover

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,15 @@
-      // табы
-
-      $('ul.works__tabs').on('click', 'li:not(.works__tab_active)', function() {
-        $(this)
-          .addClass('works__tab_active').siblings().removeClass('works__tab_active')
-          .closest('div.container').find('div.works__content').removeClass('works__content_active').eq($(this).index()).addClass('works__content_active');
-      });
-
-      // модальные окна
-
-     const more = document.querySelectorAll('.works-item__link'),
-            content = document.querySelectorAll('.works-item__content'),
-            list = document.querySelectorAll('.works-item__list'),
-            back = document.querySelectorAll('.works-item__back');
+$(document).ready(function(){
+     
+    $('ul.works__tabs').on('click', 'li:not(.works__tab_active)', function() {
+    $(this)
+        .addClass('works__tab_active').siblings().removeClass('works__tab_active')
+        .closest('div.container').find('div.works__content').removeClass('works__content_active').eq($(this).index()).addClass('works__content_active');
+    });
+      
+    const more = document.querySelectorAll('.works-item__link'),
+        content = document.querySelectorAll('.works-item__content'),
+        list = document.querySelectorAll('.works-item__list'),
+        back = document.querySelectorAll('.works-item__back');
 
     function toggle(elems) {
         elems.forEach((elem, i) => {
@@ -24,5 +22,35 @@
     }
     toggle(more);
     toggle(back);
+
+   const item = document.querySelectorAll('.services__item'),
+            img = document.querySelectorAll('.services__img'),
+            imgWhite = document.querySelectorAll('.services__img_white');
+
+    function switchWhite(elems) {
+        elems.forEach((elem, i) => {
+            elem.addEventListener('mouseover',  () => {
+                img[i].classList.toggle('services__img_none');
+                imgWhite[i].classList.toggle('services__img_white-show');
+            })
+        })
+        elems.forEach((elem, i) => {
+            elem.addEventListener('mouseout', () => {
+                img[i].classList.toggle('services__img_none');
+                imgWhite[i].classList.toggle('services__img_white-show');
+            })
+        })
+    }
+    switchWhite(item);
+ // недописанный jquery
+    /* $('.services__item').hover( () => {
+        $('.services__img').toggleClass('services__img_none');
+        $('.services__img_white').toggleClass('services__img_white-show');  
+       
+    }) */
+    
+}); 
      
+
+
     
